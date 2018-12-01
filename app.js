053d78c36ccf2860c15b2c2345313a5a4a4c98af
tika <yesikristianitika@gmail.com>
2018-12-01 16:58:44 +0700

    function

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
-var json = [
-    {
-        name : "Tika Yesi",
-        age : 23
-    },
-    {
-        name : "Kristiani",
-        age : 25
-    }
-];
-
-for (var i =0; i<json.length; i++){
-    console.log(json[i]);
+function MyFunction() {
+    console.log("i'm function");
 }
+MyFunction();
